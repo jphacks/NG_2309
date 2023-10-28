58eac996f3ed003123bb6b54038fab997501371d
vyuma <122613388+vyuma@users.noreply.github.com>
2023-10-28 17:19:00 +0900

    fix main.py

--- backend/main.py
+++ backend/main.py
@@ -3,4 +3,20 @@ from pathlib import Path
 from rauth import OAuth2Service
 import os
 import binascii
+base_dir = Path(__file__).parents[1]
+static_dir = base_dir / "frontend" / "static"
+
+
+app = Flask(__name__,
+            static_folder=static_dir)
+
+
+@app.route("/")
+def index():
+    return render_template("html/index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
