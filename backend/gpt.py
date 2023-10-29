4acdb73ce678ebd50eb9fcec1fcb3d0f264c7e75
“kidanikouki” <kidanikouki@gmail.com>
2023-10-29 10:03:54 +0900

    chat_content

--- backend/gpt.py
+++ backend/gpt.py
@@ -5,7 +5,7 @@ def evaluation_score(score):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "system", "content": "あなたはプログラマーやエンジニアに対して心の健康を向上させるようなプロの心理カウンセラーです。scoreはストレスレベルで0が最低100が最高でストレスレベルが高ければ高いほど危険な状態です。プロのプログラマーに対して次の作業に気持ちよく取り掛かれるような50文字くらいの言葉のコメントをくださいまた以下の一文はあなたの出力の例でこの形式以外は許しません。リラックスし、自信を持って次のプロジェクトに取り組みましょう。健康な心がクリエイティビティを支えます。"},
         ]
     )
     return response['choices'][0]['message']['content']
